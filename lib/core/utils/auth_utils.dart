635117b69f98fd66e32ff1381b990d96de5f390c
lypsisrudiansyah <rudiansyahether@gmail.com>
2020-08-18 17:09:34 +0700

    refactor: update some code

--- lib/core/utils/auth_utils.dart
+++ lib/core/utils/auth_utils.dart
@@ -3,6 +3,6 @@ class AuthUtils {
 
   //* Change the random string with your Zoomato API KEY
   Future getToken() async {
-    return "b22e7b9346febb8d371f20eed5e35445";
+    return "b2*****disensor*****";
   }
 }
